1cc94cbf3ffb1b3c14fdf5b80419d378aeddaabd
AhmedSamiir51 <88041263+AhmedSamiir51@users.noreply.github.com>
2021-07-28 13:59:48 +0200

    AirBnb

---
+++ AirbnbCRUD/Services/BookingInjection.cs
@@ -0,0 +1,97 @@
+﻿using AirbnbCRUD.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirbnbCRUD.Services
+{
+    public interface IBooking
+    {
+        List<Booking> GetAllBookings();
+        List<Booking> GetAllBookingsByPersonId(int PersonId);
+        List<Booking> GetAllBookingByHouseId(int HouseId);
+        Booking GetBooking(int BookingId);
+        Booking CreateBooking(Booking book);
+        Booking EditBooking(Booking book);
+        void DeleteBooking(int BookingId);
+        void DeleteAllBookingByHouseId(int HouseId);
+        void DeleteAllBooingByPersonId(int PersonId);
+        public bool BookingExists(int id);
+    }
+    public class BookingInjection:IBooking
+    {
+        private readonly ApplicationContext _context;
+        public BookingInjection(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public Booking CreateBooking(Booking book)
+        {
+            _context.Bookings.Add(book);
+            _context.SaveChanges();
+            return book;
+        }
+
+        public void DeleteAllBooingByPersonId(int PersonId)
+        {
+            var books = GetAllBookingsByPersonId(PersonId);
+            foreach(var book in books)
+            {
+                _context.Bookings.Remove(book);
+            }
+            _context.SaveChanges();
+        }
+
+        public void DeleteAllBookingByHouseId(int HouseId)
+        {
+            var books = GetAllBookingByHouseId(HouseId);
+            foreach(var book in books)
+            {
+                _context.Bookings.Remove(book);
+            }
+            _context.SaveChanges();
+        }
+
+        public void DeleteBooking(int BookingId)
+        {
+            var book = GetBooking(BookingId);
+            _context.Bookings.Remove(book);
+            _context.SaveChanges();
+        }
+
+        public Booking EditBooking(Booking book)
+        {
+            _context.Entry(book).State = EntityState.Modified;
+            _context.SaveChanges();
+            return book;
+        }
+
+        public List<Booking> GetAllBookingByHouseId(int HouseId)
+        {
+            var books = _context.Bookings.Where(a => a.HouseId == HouseId).ToList();
+            return books;
+        }
+
+        public List<Booking> GetAllBookings()
+        {
+            return _context.Bookings.ToList();
+        }
+
+        public List<Booking> GetAllBookingsByPersonId(int PersonId)
+        {
+            return _context.Bookings.Where(a => a.PersonId == PersonId).ToList();
+        }
+
+        public Booking GetBooking(int BookingId)
+        {
+            return _context.Bookings.Find(BookingId);
+        }
+        public bool BookingExists(int id)
+        {
+            return _context.Bookings.Any(e => e.BookingId == id);
+        }
+    }
+}
---
+++ AirbnbCRUD/Services/HousePhotoInjection.cs
@@ -0,0 +1,80 @@
+﻿using AirbnbCRUD.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirbnbCRUD.Services
+{
+    public interface IHousePhoto
+    {
+        List<HousePhoto> GetAllHousePhotos();
+        List<HousePhoto> GetHousePhotos(int id);
+        HousePhoto GetHousePhoto(int HouseId, string HousePhoto);
+        HousePhoto CreateHousePhoto(HousePhoto housePhoto);
+        HousePhoto EditHousephoto(HousePhoto housePhoto);
+        void DeleteHousePhoto(int HouseId,string HousePhoto);
+        void DeleteHousePhotos(int HouseId);
+        bool HousePhotoExists(int id,string housePhotoNumber);
+    }
+    public class HousePhotoInjection:IHousePhoto
+    {
+        private readonly ApplicationContext _context;
+        public HousePhotoInjection(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public HousePhoto CreateHousePhoto(HousePhoto housePhoto)
+        {
+            _context.HousePhotos.Add(housePhoto);
+            _context.SaveChanges();
+            return housePhoto;
+        }
+
+        public void DeleteHousePhoto(int HouseId, string HousePhoto)
+        {
+            var photo = GetHousePhoto(HouseId, HousePhoto);
+            _context.HousePhotos.Remove(photo);
+            _context.SaveChanges();
+        }
+
+        public void DeleteHousePhotos(int HouseId)
+        {
+            var photos = GetHousePhotos(HouseId);
+            foreach(var photo in photos)
+            {
+                _context.HousePhotos.Remove(photo);
+            }
+            _context.SaveChanges();
+        }
+
+        public HousePhoto EditHousephoto(HousePhoto housePhoto)
+        {
+            _context.Entry(housePhoto).State = EntityState.Modified;
+            _context.SaveChanges();
+            return housePhoto;
+        }
+
+        public List<HousePhoto> GetAllHousePhotos()
+        {
+            return _context.HousePhotos.ToList();
+        }
+
+        public HousePhoto GetHousePhoto(int HouseId, string HousePhoto)
+        {
+            return _context.HousePhotos.FirstOrDefault(a => a.HouseId == HouseId && a.HousePhotos == HousePhoto);
+        }
+
+        public List<HousePhoto> GetHousePhotos(int id)
+        {
+            return _context.HousePhotos.Where(a => a.HouseId == id).ToList();
+        }
+
+        public bool HousePhotoExists(int id, string housePhotoNumber)
+        {
+            return _context.HousePhotos.Any(e => e.HouseId == id && e.HousePhotos==housePhotoNumber);
+        }
+    }
+}
